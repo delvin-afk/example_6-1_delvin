4cd2de9b716655ee0243e405ca02108c3e464e2d
delvin-afk <97646655+delvin-afk@users.noreply.github.com>
2023-02-16 13:21:49 -0500

    Add files via upload

--- modules/code/code.h
+++ modules/code/code.h
@@ -18,6 +18,7 @@ typedef enum{
 
 void codeWrite( char* newCodeSequence );
 bool codeMatchFrom( codeOrigin_t codeOrigin );
+char* currentCodeSequence();
 
 //=====[#include guards - end]=================================================
 
